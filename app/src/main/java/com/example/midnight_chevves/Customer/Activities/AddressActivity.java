83828a0759c9edbe4dda1336b54afa559d2a6a2e
azuregitter <geromeriego@gmail.com>
2022-01-18 22:51:39 +0800

    google maps address

--- app/src/main/java/com/example/midnight_chevves/Customer/Activities/AddressActivity.java
+++ app/src/main/java/com/example/midnight_chevves/Customer/Activities/AddressActivity.java
@@ -1,32 +1,65 @@
 package com.example.midnight_chevves.Customer.Activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.midnight_chevves.R;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
 
-public class AddressActivity extends AppCompatActivity {
-    private ImageButton btnBack;
-    private MaterialButton btnSave;
-    private TextInputEditText add1, add2, add3, add4, add5;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class AddressActivity extends AppCompatActivity implements OnMapReadyCallback {
+    ImageButton btnBack;
+    Button btnSave;
+    Button btLocation;
+    TextView textView1, textView2;
+    FusedLocationProviderClient client;
+    SupportMapFragment supportMapFragment;
+    GoogleMap gMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
-        btnBack = findViewById(R.id.edit_back);
-        btnSave = findViewById(R.id.button_save_edit2);
-        add1 = findViewById(R.id.house_number);
-        add2 = findViewById(R.id.street);
-        add3 = findViewById(R.id.area);
-        add4 = findViewById(R.id.province);
-        add5 = findViewById(R.id.city);
+        btLocation = findViewById(R.id.bt_location);
+        textView1 = findViewById(R.id.text_view1);
+        textView2 = findViewById(R.id.text_view2);
+        btnBack = findViewById(R.id.address_back);
+        btnSave = findViewById(R.id.button_save_changes2);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        supportMapFragment.getMapAsync(this);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,26 +70,122 @@ public class AddressActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                save();
+                Toast.makeText(AddressActivity.this, "Address Saved!", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+            }
+        });
+
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(AddressActivity.this
+                        , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    ActivityCompat.requestPermissions(AddressActivity.this
+                            , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+                }
+            }
+        });
+
+    }
+
+    private void getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+
+                            LatLng latLng = new LatLng (location.getLatitude(),
+                                    location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("My Address");
+                            gMap.clear();
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                            googleMap.addMarker(options);
+
+                        }
+                    });
+                }
+            }
+        });
+        client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+                        Geocoder geocoder = new Geocoder(AddressActivity.this,
+                                Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 4);
+                        textView2.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Locality: </b><br></font>"
+                                        + addresses.get(0).getLocality()
+                        ));
+                        textView1.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Address: </b><br></font>"
+                                        + addresses.get(0).getAddressLine(0)
+                        ));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
-    private void save() {
-        String address1 = add1.getText().toString();
-        String address2 = add2.getText().toString();
-        String address3 = add3.getText().toString();
-        String address4 = add4.getText().toString();
-        String address5 = add5.getText().toString();
 
 
-        if (address1.isEmpty() || address2.isEmpty() ||address3.isEmpty() ||address4.isEmpty() ||address5.isEmpty()) {
-            Toast.makeText(this, "Please Fill All Fields", Toast.LENGTH_SHORT).show();
-        } else {
-            //address updated
-            Toast.makeText(this, "Address Updated", Toast.LENGTH_SHORT).show();
-            onBackPressed();
-        }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //Marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title("My Address");
+                gMap.clear();
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                gMap.addMarker(markerOptions);
+
+                Geocoder geocoder = new Geocoder(AddressActivity.this,
+                        Locale.getDefault());
+                double lat = latLng.latitude;
+                double lng = latLng.longitude;
+                List<Address> addresses = null;
+                try {
+                    addresses = geocoder.getFromLocation(
+                            lat, lng, 4);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                textView2.setText(Html.fromHtml(
+                        "<font color='#6200EE'><b>Locality: </b><br></font>"
+                                + addresses.get(0).getLocality()
+                ));
+                textView1.setText(Html.fromHtml(
+                        "<font color='#6200EE'><b>Address: </b><br></font>"
+                                + addresses.get(0).getAddressLine(0)
+                ));
+            }
+        });
     }
-
-
 }
